77eca093049a0744ad5d7b69502819e91e589ac9
SajeedShahriat <sajeed.shahriat@gmail.com>
2017-07-21 12:42:21 -0400

    comparator incomplete

--- comparator/comparator.v
+++ comparator/comparator.v
@@ -15,17 +15,47 @@ module comparator (
 ); //end of port list
 
 //---------------------------input ports-----------------------------
-
+input a;
+input b;
 
 //---------------------------output ports----------------------------
-
+output a_less_b;
+output a_equal_b;
+output a_greater_b;
 
 //-----------------------input port data type------------------------
-
+wire [1:0] a;
+wire [1:0] b;
 
 //-----------------------output port data type-----------------------
-
+reg a_less_b;
+reg a_equal_b;
+reg a_greater_b;
 
 //---------------------------RTL design------------------------------
 
-endmodule 
+always @ (a or b)
+	begin
+		if (a < b)
+			begin
+				a_less_b <= 1'b1;
+				a_equal_b <= 1'b0;
+				a_greater_b <= 1'b0;
+			end
+		else if (a == b)
+			begin
+				a_less_b <= 1'b0;
+				a_equal_b <= 1'b1;
+				a_greater_b <= 1'b0;
+			end
+
+		else if (a > b)
+			begin
+				a_less_b <= 1'b0;
+				a_equal_b <= 1'b0;
+				a_greater_b <= 1'b1;	
+			end
+
+	end
+
+endmodule //comparator
--- comparator/comparator_tb.v
+++ comparator/comparator_tb.v
@@ -5,24 +5,33 @@ All rights reserved
 This files can be reused and modified given that this copyright notice is not removed
 */
 
-module (
+module comparator_tb();
 
+reg a, b; //declare all inputs as registers
+wire a_less_b, a_equal_b, a_greater_b; //declare all outputs as wires
 
+initial
+	begin
+		$dumpfile ("comparator_wave.vcd"); //required for gtkwaveform
+		$dumpvars (0, comparator_tb); //required for gtkwaveform
 
-); //end of port list
+		$display ("time\t a b a<b a=b a>b");
+		$monitor ("%g\t %b %b %b\t %b  %b", $time, a, b, a_less_b, a_equal_b, a_greater_b);
 
-//---------------------------input ports-----------------------------
+//stimulus
 
+		#10 a <= 2'b00; b <= 2'b00;
+		#10 a <= 2'b01; b <= 2'b10;
+		#10 a <= 2'b10; b <= 2'b01;
+		#10 a <= 2'b11; b <= 2'b11;
+	end
 
-//---------------------------output ports----------------------------
-
-
-//-----------------------input port data type------------------------
-
-
-//-----------------------output port data type-----------------------
-
-
-//---------------------------RTL design------------------------------
+comparator connect_comparator (
+	a,
+	b,
+	a_less_b,
+	a_equal_b,
+	a_greater_b
+);
 
 endmodule 
